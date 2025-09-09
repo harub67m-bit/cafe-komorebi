f22c1a4e8bbaa02a35ab86480029420590d8aadc
harub67m-bit <harub67m@gmail.com>
2025-09-09 22:28:05 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript
+    function toggleMenu() {
+      const menu = document.getElementById('hamburger-menu');
+      const trigger = document.querySelector('.hamburger');
+      menu.classList.toggle('open');
+      trigger.classList.toggle('open');
+    }
+
+  
